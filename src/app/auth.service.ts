9f6dea29dc8f8a97cf84b5a8370f6879fb5b7cbc
Vishal Acharya <vish.acharya1995@gmail.com>
2021-05-05 23:16:05 +0530

    Adding authentication service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  loggedIn = false;
+  private loggedIn: boolean = false;
 
-  isAuthenticated() {
-    const promise = new Promise((resolve, _) => {
-      resolve(this.loggedIn);
-    });
-    return promise;
+  loginSuccessful = new Subject<boolean>();
+
+  constructor(private http: HttpClient) {}
+
+  isAuthenticated(): boolean {
+    return this.loggedIn;
   }
 
-  login() {
-    this.loggedIn = true;
+  login(empId: number) {
+    return this.http
+      .get<number[]>(
+        'https://hack-ideas-backend-default-rtdb.firebaseio.com/users.json'
+      )
+      .pipe(
+        map((response: number[]) => {
+          this.loggedIn = response.includes(empId);
+          this.loginSuccessful.next(this.loggedIn);
+          return this.loggedIn;
+        })
+      );
   }
 
   logout() {
     this.loggedIn = false;
+    this.loginSuccessful.next(this.loggedIn);
   }
 }
