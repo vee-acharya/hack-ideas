cc16944afbb5b4d69840882b4fb10b1e432a49f4
Vishal Acharya <vish.acharya1995@gmail.com>
2021-05-10 12:12:57 +0530

    Restructured ChallengeEdit component with test cases

--- src/app/challenges/challenge-edit/challenge-edit.component.spec.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from 'src/app/auth.service';
+import { ChallengesService } from '../challenges.service';
 
 import { ChallengeEditComponent } from './challenge-edit.component';
 
@@ -8,9 +12,10 @@ describe('ChallengeEditComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChallengeEditComponent ]
-    })
-    .compileComponents();
+      declarations: [ChallengeEditComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [ChallengesService, AuthService],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +26,45 @@ describe('ChallengeEditComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call initForm function after component is initialized', () => {
+    const fixture = TestBed.createComponent(ChallengeEditComponent);
+    const component = fixture.componentInstance;
+    expect(component.form).toBeUndefined();
+    fixture.detectChanges();
+    expect(component.form).toBeDefined();
+  });
+
+  it('should not be able to call onSubmit when form is invalid', () => {
+    component.form.controls['title'].setValue('');
+    component.form.controls['description'].setValue('');
+    fixture.detectChanges();
+    expect(
+      fixture.nativeElement.querySelector('.app-primary-btn').disabled
+    ).toBeTruthy();
+
+    component.form.controls['title'].setValue('Test title');
+    component.form.controls['description'].setValue('');
+    fixture.detectChanges();
+    expect(
+      fixture.nativeElement.querySelector('.app-primary-btn').disabled
+    ).toBeTruthy();
+
+    component.form.controls['title'].setValue('');
+    component.form.controls['description'].setValue('Test description');
+    fixture.detectChanges();
+    expect(
+      fixture.nativeElement.querySelector('.app-primary-btn').disabled
+    ).toBeTruthy();
+  });
+
+  it('should be able to call onSubmit when form is valid', () => {
+    component.form.controls['title'].setValue('Test title');
+    component.form.controls['description'].setValue('Test description');
+    fixture.detectChanges();
+    expect(
+      fixture.nativeElement.querySelector('.app-primary-btn').disabled
+    ).toBeFalsy();
   });
 });
--- src/app/challenges/challenge-edit/challenge-edit.component.ts
+++ src/app/challenges/challenge-edit/challenge-edit.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Params } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Challenge } from '../challenge.interface';
+import { ChallengesService } from '../challenges.service';
 
 @Component({
   selector: 'app-challenge-edit',
@@ -9,22 +10,30 @@ import { Challenge } from '../challenge.interface';
   styleUrls: ['./challenge-edit.component.css'],
 })
 export class ChallengeEditComponent implements OnInit {
-  form: FormGroup = new FormGroup({});
-  challengeId: number = -1;
+  form!: FormGroup;
+  challengeId: string = '';
   editMode: boolean = false;
-  challengeToBeEdited: Challenge = {
-    title: '',
-    description: '',
-    tags: [],
-  };
+  challengeToBeEdited: Challenge = new Challenge();
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private challengesService: ChallengesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
-      this.challengeId = +params.id + 1;
+      this.challengeId = params.id;
       this.editMode = this.challengeId ? true : false;
-      // fetch challengeToBeEdited
+      if (this.editMode) {
+        this.challengesService
+          .fetchChallengeById(this.challengeId)
+          .subscribe(response => {
+            const challenge = { ...response, id: this.challengeId };
+            this.challengeToBeEdited = challenge as Challenge;
+            this.initForm();
+          });
+      }
     });
     this.initForm();
   }
@@ -32,30 +41,39 @@ export class ChallengeEditComponent implements OnInit {
   initForm() {
     let title = '';
     let description = '';
-    let tags = new FormArray([]);
 
     if (this.editMode) {
       title = this.challengeToBeEdited.title;
       description = this.challengeToBeEdited.description;
-      if (this.challengeToBeEdited.tags) {
-        this.challengeToBeEdited.tags.forEach((tag: string) => {
-          tags.push(new FormControl(tag, Validators.required));
-        });
-      }
     }
 
     this.form = new FormGroup({
       title: new FormControl(title, Validators.required),
       description: new FormControl(description, Validators.required),
-      tags,
     });
   }
 
-  getTags() {
-    return (<FormArray>this.form.get('tags')).controls;
-  }
-
   onSubmit() {
-    console.log(this.form);
+    const challenge = {
+      title: this.form.value.title,
+      description: this.form.value.description,
+      tags: this.challengeToBeEdited.tags,
+      creationDate: 0,
+      upvotes: this.challengeToBeEdited.upvotes,
+      upvotedBy: this.challengeToBeEdited.upvotedBy,
+    };
+    if (this.editMode) {
+      (challenge.creationDate = this.challengeToBeEdited.creationDate),
+        this.challengesService
+          .updateChallenge(this.challengeId, challenge)
+          .subscribe(() => {
+            this.router.navigate(['/challenges']);
+          });
+    } else {
+      (challenge.creationDate = new Date().getTime()),
+        this.challengesService.addChallenge(challenge).subscribe(() => {
+          this.router.navigate(['/challenges']);
+        });
+    }
   }
 }
